4740e38f5aee88017b6bbe26c93241d3f54512f9
Dailsonx <dailsonamaral1530@gmail.com>
2025-02-04 18:09:50 -0300

    forms

---
+++ FORMULARIO/src/javascript/script.js
@@ -0,0 +1,19 @@
+
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const lastname = document.getElementById("last-name");
+const birthdate = document.getElementById("birthdate");
+const email = document.getElementById("email");
+const tel = document.getElementById("tel");
+const telefone = document.getElementById("telefone");
+const female = document.getElementById("famale");
+const male = document.getElementById("male");
+const other = document.getElementById("other");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  function checkInputtel(){
+    const telValue = tel.value;
+  }
+})
